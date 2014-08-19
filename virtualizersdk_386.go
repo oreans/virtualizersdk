1ed56c6020fcb573d0563f394a00015799246dfa
rahucha <rahucha@oreans.com>
2014-08-19 13:33:16 +0200

    Create virtualizersdk_386.go

---
+++ virtualizersdk_386.go
@@ -0,0 +1,63 @@
+// ******************************************************************************
+// Header: virtualizersdk_386.go
+// Description: Go macros definitions
+//
+// Author/s: Oreans Technologies 
+// (c) 2014 Oreans Technologies
+//
+// --- File generated automatically from Oreans VM Generator (19/8/2014) ---
+// ******************************************************************************
+
+package virtualizersdk;
+
+import (
+	"fmt"
+)
+
+// ******************************************************************************
+//                               Constants definition
+// ******************************************************************************
+
+const TIGER_WHITE_START                              = "CustomVM00000100_Start";
+const TIGER_WHITE_END                                = "CustomVM00000100_End";
+const TIGER_RED_START                                = "CustomVM00000101_Start";
+const TIGER_RED_END                                  = "CustomVM00000101_End";
+const TIGER_BLACK_START                              = "CustomVM00000102_Start";
+const TIGER_BLACK_END                                = "CustomVM00000102_End";
+const FISH_WHITE_START                               = "CustomVM00000106_Start";
+const FISH_WHITE_END                                 = "CustomVM00000106_End";
+const FISH_RED_START                                 = "CustomVM00000108_Start";
+const FISH_RED_END                                   = "CustomVM00000108_End";
+const FISH_BLACK_START                               = "CustomVM00000110_Start";
+const FISH_BLACK_END                                 = "CustomVM00000110_End";
+const PUMA_WHITE_START                               = "CustomVM00000112_Start";
+const PUMA_WHITE_END                                 = "CustomVM00000112_End";
+const PUMA_RED_START                                 = "CustomVM00000114_Start";
+const PUMA_RED_END                                   = "CustomVM00000114_End";
+const PUMA_BLACK_START                               = "CustomVM00000116_Start";
+const PUMA_BLACK_END                                 = "CustomVM00000116_End";
+const SHARK_WHITE_START                              = "CustomVM00000118_Start";
+const SHARK_WHITE_END                                = "CustomVM00000118_End";
+const SHARK_RED_START                                = "CustomVM00000120_Start";
+const SHARK_RED_END                                  = "CustomVM00000120_End";
+const SHARK_BLACK_START                              = "CustomVM00000122_Start";
+const SHARK_BLACK_END                                = "CustomVM00000122_End";
+const MUTATE_ONLY_START                              = "Mutate_Start";
+const MUTATE_ONLY_END                                = "Mutate_End";
+
+
+// ******************************************************************************
+//                                 Bridge Function definition
+// ******************************************************************************
+
+func Macro(str string) int {
+	if (str == "Hi Bridge") {
+		for i := 0; i < 5; i++ {
+			  fmt.Printf("text", str[i]);
+		}
+		for i := 0; i < 5; i++ {
+			fmt.Printf("text1", str[i + 1]);
+		}
+	}
+	return  int(str[0]);
+}
